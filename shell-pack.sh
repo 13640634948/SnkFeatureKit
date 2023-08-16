3ec072f10a00b2ae621525f25336e4cb754d7a29
乐开霖 <7941324@qq.com>
2023-08-16 20:56:56 +0800

    sh脚本

--- shell-pack.sh
+++ shell-pack.sh
@@ -1,6 +1,6 @@
-version="1.1.3"
+key=$1
+version=$2
 directory="NugetOutput"
-key=""
 source="https://api.nuget.org/v3/index.json"
 
 nuget pack SnkFeatureKit.Asynchronous/SnkFeatureKit.Asynchronous.csproj -Version $version -OutputDirectory $directory/$version
@@ -8,8 +8,7 @@ nuget pack SnkFeatureKit.ContentDelivery/SnkFeatureKit.ContentDelivery.csproj -V
 nuget pack SnkFeatureKit.Logging/SnkFeatureKit.Logging.csproj -Version $version -OutputDirectory $directory/$version
 nuget pack SnkFeatureKit.Patcher/SnkFeatureKit.Patcher.csproj -Version $version -OutputDirectory $directory/$version
 
-nuget push $directory/$version/SnkFeatureKit.Asynchronous.$version.nupkg $key -Source $source
-nuget push $directory/$version/SnkFeatureKit.ContentDelivery.$version.nupkg $key -Source $source
-nuget push $directory/$version/SnkFeatureKit.Logging.$version.nupkg $key -Source $source
-nuget push $directory/$version/SnkFeatureKit.Patcher.$version.nupkg $key -Source $source
-
+#nuget push $directory/$version/SnkFeatureKit.Asynchronous.$version.nupkg $key -Source $source
+#nuget push $directory/$version/SnkFeatureKit.ContentDelivery.$version.nupkg $key -Source $source
+#nuget push $directory/$version/SnkFeatureKit.Logging.$version.nupkg $key -Source $source
+#nuget push $directory/$version/SnkFeatureKit.Patcher.$version.nupkg $key -Source $source
