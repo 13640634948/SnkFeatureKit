8ce04db91e786456fe5de80cc15b0f434fe12f41
乐开霖 <7941324@qq.com>
2023-08-16 18:33:35 +0800

    增加自动Pack+Push脚本

---
+++ shell-pack.sh
@@ -0,0 +1,15 @@
+version="1.1.3"
+directory="NugetOutput"
+key=""
+source="https://api.nuget.org/v3/index.json"
+
+nuget pack SnkFeatureKit.Asynchronous/SnkFeatureKit.Asynchronous.csproj -Version $version -OutputDirectory $directory/$version
+nuget pack SnkFeatureKit.ContentDelivery/SnkFeatureKit.ContentDelivery.csproj -Version $version -OutputDirectory $directory/$version
+nuget pack SnkFeatureKit.Logging/SnkFeatureKit.Logging.csproj -Version $version -OutputDirectory $directory/$version
+nuget pack SnkFeatureKit.Patcher/SnkFeatureKit.Patcher.csproj -Version $version -OutputDirectory $directory/$version
+
+nuget push $directory/$version/SnkFeatureKit.Asynchronous.$version.nupkg $key -Source $source
+nuget push $directory/$version/SnkFeatureKit.ContentDelivery.$version.nupkg $key -Source $source
+nuget push $directory/$version/SnkFeatureKit.Logging.$version.nupkg $key -Source $source
+nuget push $directory/$version/SnkFeatureKit.Patcher.$version.nupkg $key -Source $source
+
