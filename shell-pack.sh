6faf9235868887be8ef6a4698d10bf7f725bab04
乐开霖 <7941324@qq.com>
2023-08-22 16:46:20 +0800

    增加build命令

--- shell-pack.sh
+++ shell-pack.sh
@@ -8,13 +8,14 @@ source="https://api.nuget.org/v3/index.json"
 
 proj_array[0]="SnkFeatureKit.Asynchronous"
 proj_array[1]="SnkFeatureKit.ContentDelivery"
-proj_array[2]="SnkFeatureKit.Logging"
-proj_array[3]="SnkFeatureKit.Patcher"
+#proj_array[2]="SnkFeatureKit.Logging"
+proj_array[2]="SnkFeatureKit.Patcher"
 
 
 for (( i = 0; i < ${#proj_array[@]}; i++ )); do
 	proj_name=${proj_array[$i]}
 #	nuget pack $proj_name/$proj_name.csproj -Version $version -OutputDirectory $directory/$version -Properties Configuration=$configuration
+	dotnet build $proj_name/$proj_name.csproj -c:$configuration
 	dotnet pack -o $directory/$version -p:PackageVersion=$version -c:$configuration
 done
 
@@ -24,7 +25,7 @@ if [ -n "$key" ]; then
 	for (( i = 0; i < ${#proj_array[@]}; i++ )); do
 		proj_name=${proj_array[$i]}
 		#nuget push $directory/$version/$proj_name.$version.nupkg $key -Source $source
-		dotnet nuget push $directory/$version/$proj_name.$version.nupkg --api-key $key --source $source
+		dotnet nuget push $directory/$version/$proj_name.$version.nupkg -k:$key -s:$source
 	done
 fi
 
